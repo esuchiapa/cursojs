d4664ebb48ad62688ff5fed5a7c64733c5fc2791
Enrique <esuchiapa@gmail.com>
2024-01-08 13:48:41 -0600

    NA

---
+++ js/math.js
@@ -0,0 +1,6 @@
+
+let aleatorio=Math.random()*100;
+
+aleatorio=Math.floor(aleatorio);
+
+document.write(aleatorio)
--- js/metodosarrays.js
+++ js/metodosarrays.js
@@ -21,12 +21,52 @@ document.write(nombres.reverse());
 nombres.unshift("Adriana","María","Rocío");
 
 document.write(`<br> ${nombres}`);
-
+document.write("<br>");
 //Ordenar arreglo
 let numero=[2,5,1,9,5,7];
 numero.sort();
 
 document.write(numero);
-
+document.write("<br>");
 //Eliminar elementos desde una posición específica
-numero.slice(3,2);
+numero.splice(3,2);
+document.write(numero);
+
+document.write("<br>");
+
+//Métodos accesores
+//Join covierte el arreglo en una cadena de texto
+let frutas=["platano", "manzana", "mandarina","fresa"];
+document.write(frutas);
+document.write("<br>");
+
+let frutasJoin=frutas.join("-");
+
+document.write(frutasJoin);
+
+//Slice devuelve una parte del array dentro de un nuevo arreglo empezando por inicio hasta fin
+frutasJoin=frutas.slice(1,3);
+document.write("<br>");
+
+document.write(frutasJoin);
+//Include sirve para ver si un elemento está en el arreglo
+document.write("<br>");
+document.write(frutas.includes("manzana"));
+document.write(frutas.indexOf("fresa"));
+
+
+//De repetición de arreglos
+
+//Filter es un bucle
+document.write("<br>");
+frutas.filter(fruta=> document.write ("Fruta: "+fruta+"<br>"));
+
+
+//For each
+document.write("<br>");
+frutas.forEach(fruta=> document.write ("Fruta: "+fruta+"<br>"));
+
+//Filter crea un nuevo array con las propiedades especificadas
+
+resultado=frutas.filter(fruta=>frutas.length>2);
+document.write(resultado);
